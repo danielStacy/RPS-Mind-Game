33a4005eda1a23592eff87441fa90ef130a592b4
Daniel Stacy <drs.stacy@outlook.com>
2025-05-28 15:16:00 +0800

    Initial commit
    Created project structure.
    Created package.json and webpack config with necessary dev-dependencies.
    Linked and verified main files.
    Created basic style.css file with appropriate initial property values.
    Created boilerplate template.html.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock Paper Scissors, but Different
+
+A single-player rock-paper-scissors mind game.
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style.css";
+
+// Verified. This sure does print in the console of dist/index.html.
+// Feel free to delete when needed.
+console.log("This should print in the console.");
