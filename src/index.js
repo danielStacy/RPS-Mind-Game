0933401b2d5c2a7c3ae7bc5015a8cce548730cb6
Daniel Stacy <drs.stacy@outlook.com>
2025-05-29 16:35:33 +0800

    Implemented basic test version of game
    Entry point (index.js) currently contains a basic mockup of the game logic.
    Appears to be working fine, just needs to be modularised and expanded.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import "./style.css";
 import { GameCard } from "./game-logic/GameCard.js";
+import { Validator } from "./game-logic/Validator.js";
+import { RPSRound } from "./game-logic/RPSRound.js";
+import {
+  getRandomHand,
+  getRandomDirective,
+  matchResultDirective,
+} from "./game-logic/helpers.js";
 
-const gameCard = new GameCard("rock", "win");
+let playerHand, computerHand, directive, card, round, result;
 
-const main = document.querySelector(".game-board");
-main.appendChild(gameCard.card);
+const gameBoard = document.querySelector(".game-board");
+const btnNodeList = document.querySelectorAll(".btn-hand");
+btnNodeList.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    playerHand = btn.textContent;
+    playRound();
+  });
+});
+
+setupRound();
+
+function setupRound() {
+  gameBoard.innerHTML = "";
+  computerHand = getRandomHand();
+  directive = getRandomDirective();
+  card = new GameCard(computerHand, directive);
+  gameBoard.appendChild(card.card);
+}
+
+function playRound() {
+  round = new RPSRound(playerHand, computerHand);
+  result = matchResultDirective(round.result, directive);
+  disableButtons(true);
+  result
+    ? (card.card.style.backgroundColor = "lightgreen")
+    : (card.card.style.backgroundColor = "red");
+  setTimeout(() => {
+    setupRound();
+    disableButtons(false);
+  }, 1000);
+}
+
+function disableButtons(disabled = true) {
+  btnNodeList.forEach((btn) => (btn.disabled = disabled));
+}
